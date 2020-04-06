de97d9cdff82987d46189bade2e7d29f5c09b421
tcm5343 <tcm5343@psu.edu>
2020-04-05 21:33:45 -0400

    changed charts to display on frame instead of new window

---
+++ StudentRegistration/StudentRegistration/src/studentregistration/StudentRecord.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentregistration;
+
+/**
+ *
+ * @author miller
+ */
+public class StudentRecord {
+    private String fName;
+    private String lName;
+    private String degreeStatus;
+    private String major;
+    
+    // default constructor
+    public StudentRecord(){
+    }
+    
+    public StudentRecord(String fName, String lName, String degreeStatus, String major){
+        this.fName = fName;
+        this.lName = lName;
+        this.degreeStatus = degreeStatus;
+        this.major = major;
+    }
+
+    public String getFirstName() {
+        return fName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.fName = firstName;
+    }
+
+    public String getLastName() {
+        return lName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lName = lastName;
+    }
+
+    public String getDegreeStatus() {
+        return degreeStatus;
+    }
+
+    public void setDegreeStatus(String degreeStatus) {
+        this.degreeStatus = degreeStatus;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    
+    @Override
+    // is there a reason tabs do not work here?
+    public String toString(){
+        String result = this.getFirstName() + " " + this.getLastName() + "\t " 
+                + this.getDegreeStatus() + "\t " + this.getMajor();
+        return result;
+    }
+    
+}
